7b67368f07feb44a5469e2691c9e4a2db7317f70
Manuel Casanova <manucasanova@hotmail.com>
2022-09-19 11:54:56 -0700

    Redoing step by step to find error

--- server/controllers/refreshTokenController.js
+++ server/controllers/refreshTokenController.js
@@ -4,12 +4,28 @@ const jwt = require('jsonwebtoken');
 const handleRefreshToken = async (req, res) => {
   const cookies = req.cookies
   if (!cookies?.jwt) return res.sendStatus(401);
-
   const refreshToken = cookies.jwt;
+  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
 
   //See if the username exists
   const foundUser = await User.findOne({refreshToken}).exec();
-  if (!foundUser) return res.sendStatus(403); //403 Forbidden
+
+
+//detected refresh token reuse
+  if (!foundUser) {
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, decoded) => {
+        if (err) return res.sendStatus(403);
+        const hackedUser = await User.findOne({username: decoded.username}).exec();
+        hackedUser.refreshToken = [];
+        const result = await hackedUser.save();
+        console.log(result);
+      }
+    )
+    return res.sendStatus(403); //403 Forbidden
+  }
 
   //evaluate jwt
   jwt.verify(
